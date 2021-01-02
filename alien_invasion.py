f036ea40d0060c79040d462c649c36285f617bae
Joao Carlos <sijoaocarlos608@gmail.com>
2021-01-02 13:37:18 -0300

    Criei um alienigena

---
+++ alien_invasion.py
@@ -0,0 +1,43 @@
+import pygame
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Cria o botão play
+    play_button = Button(ai_settings, screen, "Play")
+
+    # Cria uma instância para armazenar dados estatísticos do jogo e cria painel de pontuação
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # Cria uma espaçonave, um grupo de projéteis e um grupo de alienígenas
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Inicia o laço principal do jogo
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:   
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+
+
+run_game()
